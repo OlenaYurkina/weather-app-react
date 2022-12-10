516f0c3a42f605fb5d1353a33be2013d0ae8dbf3
OlenaYurkina <photoyurkina@gmail.com>
2022-12-10 12:22:06 -0500

    Added link to GirHub

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function Footer() {
-  return <div className="Footer">Open source code. Coded by Olena Yurkina</div>;
+  return (
+    <div className="Footer">
+      <a
+        href="https://github.com/OlenaYurkina/weather-app-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        Open source code.{" "}
+      </a>{" "}
+      Coded by Olena Yurkina
+    </div>
+  );
 }
 
 export default Footer;
