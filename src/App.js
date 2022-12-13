ce8231ab7d0099f9058f6aa65aa303c3f5edff13
OlenaYurkina <photoyurkina@gmail.com>
2022-12-13 11:49:10 -0500

    Changed structure of the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Main from "./Main";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <p>Weather App in React</p>
+
+      <Main />
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,21 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Header from "./Header";
-import Form from "./Form";
-import Main from "./Main";
-import Days from "./Days";
-import Footer from "./Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Header />
-    <Form />
-    <Main />
-    <Days />
-    <Footer />
   </React.StrictMode>
 );
 
